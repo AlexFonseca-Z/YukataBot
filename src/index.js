88d11382c44d0dc4d23d1d79bdd8ec17fd3d05a9
Alex Fonseca <alex.fonseca.yw@hotmail.com>
2023-09-20 03:38:51 +0100

    New commands Hi and Bye

--- src/index.js
+++ src/index.js
@@ -11,7 +11,30 @@ client.on("ready", (x) => {
     .setName('ping')
     .setDescription('This is a ping command!');
 
+    const hello = new SlashCommandBuilder()
+    .setName('hello')
+    .setDescription('This is a hello command!');
+
+    const bye = new SlashCommandBuilder()
+    .setName('bye')
+    .setDescription('This is a bye command!');
+
     client.application.commands.create(ping);
+    client.application.commands.create(hello);
+    client.application.commands.create(bye, 1104250328255307776);
+})
+
+client.on('interactionCreate',  (interaction) => {
+    if(!interaction.isChatInputCommand()) return;
+    if(interaction.commandName==='ping') {
+        interaction.reply('Pong!');
+    }
+    if(interaction.commandName==='hello') {
+        interaction.reply('Hai!');
+    }
+    if(interaction.commandName==='bye') {
+        interaction.reply('Bai!');
+    }
 })
 
 client.login(process.env.TOKEN);
