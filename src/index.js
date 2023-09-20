e35c335c0bb050273f8a7007e4ac418793754d62
Alex Fonseca <alex.fonseca.yw@hotmail.com>
2023-09-20 03:13:49 +0100

    Test ping command

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 require('dotenv').config();
 
-const {Client, GatewayIntentBits, EmbedBuilder, PermissionsBitField, Permissions} = require('discord.js');
+const {Client, GatewayIntentBits, EmbedBuilder, SlashCommandBuilder, PermissionsBitField, Permissions} = require('discord.js');
 const client = new Client({intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]});
 
 client.on("ready", (x) => {
     console.log(`${x.user.tag} is ready!`);
     client.user.setActivity(`I'm in danger`);
+
+    const ping = new SlashCommandBuilder()
+    .setName('ping')
+    .setDescription('This is a ping command!');
+
+    client.application.commands.create(ping);
 })
 
 client.login(process.env.TOKEN);
