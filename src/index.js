93aaf12f44e6aae51dca105e311bb78ab406b38c
Alex Fonseca <alex.fonseca.yw@hotmail.com>
2023-09-20 03:09:15 +0100

    Bot default setup without a purpose

---
+++ src/index.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+
+const {Client, GatewayIntentBits, EmbedBuilder, PermissionsBitField, Permissions} = require('discord.js');
+const client = new Client({intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]});
+
+client.on("ready", (x) => {
+    console.log(`${x.user.tag} is ready!`);
+    client.user.setActivity(`I'm in danger`);
+})
+
+client.login(process.env.TOKEN);
